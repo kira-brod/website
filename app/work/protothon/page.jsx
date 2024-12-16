89dd7498704fc222801aff23790e91a224bedcfd
Kira Brodsky <kira.b@outlook.com>
2024-12-15 19:39:25 -0800

    fix

--- app/work/protothon/page.jsx
+++ app/work/protothon/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import NavBar from "@/app/components/NavBar"conta
+import NavBar from "@/app/components/NavBar"
 import Image from "next/image"
 import Link from "next/link"
 import { motion } from "framer-motion"
