e7f61a3f952e9d70ca80b2249da6269032e92acd
Kira <kira.b@outlook.com>
2024-12-15 18:48:00 -0800

    install

--- app/work/protothon/page.jsx
+++ app/work/protothon/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import NavBar from "../app/components/NavBar"
+import NavBar from "@/app/components/NavBar"
 import Container from "@/app/components/Container"
 import Image from "next/image"
 import Link from "next/link"
