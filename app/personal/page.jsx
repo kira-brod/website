d57db29035a133640e5a588908d49e2e598bfd35
Kira Brodsky <kira.b@outlook.com>
2024-12-17 18:24:50 -0800

    image

--- app/personal/page.jsx
+++ app/personal/page.jsx
@@ -29,16 +29,23 @@ export default function Personal() {
                 </div>
 
                 <div className="bg-gradient-to-r from-[#CD85B7]/25 from-10% via-[#CD8595]/50 via-30% to-[#C085CD]/25 to-90%  mt-16  rounded-2xl mx-10">
-                    <div className="mx-10 mt-14 lg:mx-auto lg:mt-0 max-w-5xl lg:pr-44">
-                        <div>
-                            <p className=" text-7xl mt-44 font-bold leading-[3.5rem]">
-                                Hi, I&apos;m Kira!
-                            </p>
-                        </div>
-                        <div className="mt-20 mb-20 grid grid-cols-1 lg:grid-cols-4">
-                            <div className="col-span-2">
-                                <p className="mb-5">Aside from being interested in UX/UI Design, I love all things arts and crafts. From crocheting to studying abroad in London for art history, I am a sucker for a good piece of art.</p>
-                                <p>I also go bouldering on occasion!</p>
+                    <div className="mx-10 mt-14 lg:mx-auto lg:mt-0 max-w-5xl ">
+                        <div className="grid grid-cols-1 md:grid-cols-2">
+                            <div className="">
+                                <div>
+                                    <p className=" text-7xl mt-44 font-bold leading-[3.5rem]">
+                                        Hi, I&apos;m Kira!
+                                    </p>
+                                </div>
+                                <div className="mt-20 mb-20 grid grid-cols-1 lg:grid-cols-4">
+                                    <div className="col-span-3">
+                                        <p className="mb-5">Aside from being interested in UX/UI Design, I love all things arts and crafts. From crocheting to studying abroad in London for art history, I am a sucker for a good piece of art.</p>
+                                        <p>I also go bouldering on occasion!</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="content-end">
+                                <Image src="/img/bunny.png" className="" alt="under construction sign" width={800} height={800} />
                             </div>
                         </div>
                     </div>
